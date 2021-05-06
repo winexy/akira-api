c7258f5a4fdca44dbfcb4a803ff09313e2662a89
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-06 16:27:45 +0300

    chore: configure objection with knex

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB
+  },
+  migrations: {
+    table: 'migrations',
+    directory: './src/migrations'
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import {Module} from '@nestjs/common'
+import {ObjectionModule} from '@willsoto/nestjs-objection'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ObjectionModule.register({
+      config: {
+        client: 'pg',
+        useNullAsDefault: true,
+        connection: {
+          database: process.env.PG_DB,
+          host: process.env.PG_HOST,
+          user: process.env.PG_USER,
+          password: process.env.PG_PASSWORD
+        },
+        migrations: {
+          tableName: 'migrations'
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
