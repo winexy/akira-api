09ed0812fd120f21730212729fb74d65af4ccbca
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-08 21:14:03 +0300

    feat: add firebase admin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import {ConfigService} from '@nestjs/config'
 import {NestFactory} from '@nestjs/core'
+import firebase, {ServiceAccount} from 'firebase-admin'
 import {AppModule} from './app.module'
+import * as serviceAccount from '../firebase-service-account.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = app.get<ConfigService>(ConfigService)
   const port = config.get('PORT', 3000)
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount as ServiceAccount)
+  })
 
   await app.listen(port)
 }
