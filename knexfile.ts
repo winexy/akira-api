1ef14f6459bff9e878ec235027c8bdab55c0e1c1
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-09 15:24:05 +0300

    chore: run migrations

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,10 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
   },
   migrations: {
     table: 'migrations',
