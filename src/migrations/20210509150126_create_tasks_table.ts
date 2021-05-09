46271a5a650c164ca1c754491eebd42237a811b0
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-05-09 21:27:29 +0300

    fix: update tasks table migration

--- src/migrations/20210509150126_create_tasks_table.ts
+++ src/migrations/20210509150126_create_tasks_table.ts
@@ -4,7 +4,7 @@ const tableName = 'tasks'
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(tableName, t => {
-    t.increments()
+    t.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
     t.string('title').notNullable()
     t.string('author_uid').notNullable()
     t.boolean('is_completed').notNullable().defaultTo(false)
