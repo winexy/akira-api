4307f9c5758b36998eac3d551f1995791e287e9b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-07 22:55:48 +0300

    refactor: remvoe useless taskId param

--- src/modules/checklist/checklist.controller.ts
+++ src/modules/checklist/checklist.controller.ts
@@ -1,19 +1,15 @@
-import {Body, Controller, Param, Post} from '@nestjs/common'
+import {Body, Controller, Post} from '@nestjs/common'
 import {ChecklistService} from './checklist.service'
 import {CreateTodoDto, createTodoDtoSchema} from './create-todo.dto'
 import {FujiPipe} from '../../pipes/fuji.pipe'
-import {TaskIdT} from '../tasks/task.model'
 
 @Controller('checklist')
 export class ChecklistController {
   constructor(private readonly checklistService: ChecklistService) {}
 
-  @Post('/:taskId')
-  async addTodo(
-    @Param('taskId') taskId: TaskIdT,
-    @Body(new FujiPipe(createTodoDtoSchema)) body: CreateTodoDto
-  ) {
-    const result = await this.checklistService.addTodo({...body, taskId})
+  @Post()
+  async addTodo(@Body(new FujiPipe(createTodoDtoSchema)) body: CreateTodoDto) {
+    const result = await this.checklistService.addTodo(body)
 
     if (result.isLeft()) {
       throw result.value
