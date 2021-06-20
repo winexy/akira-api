8acb8a402cb3346a828fca27725edbc31eec3741
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-20 12:29:57 +0300

    fix: task list repo registration

--- src/modules/task-lists/task-lists.module.ts
+++ src/modules/task-lists/task-lists.module.ts
@@ -3,10 +3,11 @@ import {ObjectionModule} from '@willsoto/nestjs-objection'
 import {TaskListsController} from './task-lists.controller'
 import {TaskListsService} from './task-lists.service'
 import {TaskListModel} from './task-list.model'
+import {TaskListRepo} from './task-lists.repository'
 
 @Module({
   imports: [ObjectionModule.forFeature([TaskListModel])],
   controllers: [TaskListsController],
-  providers: [TaskListsService]
+  providers: [TaskListsService, TaskListRepo]
 })
 export class TaskListsModule {}
