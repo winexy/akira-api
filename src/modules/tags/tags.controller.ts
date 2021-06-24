3aa5073a5ed194aff399222b0e57cdeb7229aa68
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-24 23:34:52 +0300

    feat: add auth guard

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,9 +1,11 @@
-import {Body, Controller, HttpCode, Post} from '@nestjs/common'
+import {Body, Controller, HttpCode, Post, UseGuards} from '@nestjs/common'
 import {User} from 'src/decorators/user.decorator'
 import {FujiPipe} from 'src/pipes/fuji.pipe'
 import {createTagSchema, CreateTagDto} from './tag.model'
 import {TagsService} from './tags.service'
+import {AuthGuard} from '../../auth.guard'
 
+@UseGuards(AuthGuard)
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
