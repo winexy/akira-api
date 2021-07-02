4e2d318a939a9e5dc60d509958c2daa54629e60b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-03 00:07:37 +0300

    feat: add unique constraint

---
+++ src/migrations/20210702211955_add_unique_constraint_for_lists.ts
@@ -0,0 +1,14 @@
+import * as Knex from 'knex'
+
+const table = 'task_lists'
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(table, t => {
+    t.unique(['author_uid', 'title'])
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable(table, t => {
+    t.dropUnique(['author_uid', 'title'])
+  })
+}
