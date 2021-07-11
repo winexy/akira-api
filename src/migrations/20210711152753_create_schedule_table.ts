e679dc9a3b623f82eb95455999ceaa3ac9bac75d
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-07-11 17:51:20 +0300

    fix: fix migration

--- src/migrations/20210711152753_create_schedule_table.ts
+++ src/migrations/20210711152753_create_schedule_table.ts
@@ -5,10 +5,10 @@ const table = 'scheduled_tasks'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(table, t => {
     t.increments()
-    t.uuid('task_id')
-    t.date('date')
+    t.uuid('task_id').notNullable()
+    t.date('date').notNullable()
 
-    t.foreign('task_id').references('tasks.task_id').onDelete('CASCADE')
+    t.foreign('task_id').references('tasks.id').onDelete('CASCADE')
   })
 }
 
