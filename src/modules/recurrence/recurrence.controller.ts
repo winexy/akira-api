c991934e91bea4ab666c499b548c2bdbe474d466
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-27 23:25:16 +0300

    feat: add basic controller

--- src/modules/recurrence/recurrence.controller.ts
+++ src/modules/recurrence/recurrence.controller.ts
@@ -1,12 +1,29 @@
-import {Controller, Get, Post, Patch, Param, Delete} from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Body
+} from '@nestjs/common'
 import {RecurrenceService} from './recurrence.service'
+import {AuthGuard} from '../../auth.guard'
+import {FujiPipe} from '../../pipes/fuji.pipe'
+import {ruleSchema, RuleSchema} from './recurrence.model'
 
+@UseGuards(AuthGuard)
 @Controller('recurrence')
 export class RecurrenceController {
   constructor(private readonly recurrenceService: RecurrenceService) {}
 
-  @Post()
-  create() {
+  @Post(':taskId')
+  create(
+    @Param('taskId') taskId: string,
+    @Body(FujiPipe.of(ruleSchema)) payload: RuleSchema
+  ) {
+    console.log({taskId, payload})
     return this.recurrenceService.create()
   }
 
