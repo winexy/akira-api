fdc45121f80bacf6083ed833617bb35901a632e3
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-28 21:38:24 +0300

    feat: import modules

--- src/modules/recurrence/recurrence.module.ts
+++ src/modules/recurrence/recurrence.module.ts
@@ -1,9 +1,13 @@
 import {Module} from '@nestjs/common'
+import {ObjectionModule} from '@willsoto/nestjs-objection'
+import {TasksModule} from '../tasks/tasks.module'
 import {RecurrenceService} from './recurrence.service'
 import {RecurrenceController} from './recurrence.controller'
 import {RecurrenceRepo} from './recurrence.repository'
+import {RecurrenceModel} from './recurrence.model'
 
 @Module({
+  imports: [ObjectionModule.forFeature([RecurrenceModel]), TasksModule],
   controllers: [RecurrenceController],
   providers: [RecurrenceService, RecurrenceRepo]
 })
