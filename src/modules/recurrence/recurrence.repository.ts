74d8b618635ba6da44a84297c9fabe10d0fcba12
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-30 00:35:18 +0300

    feat: add findByNextDateWithGraphFetched method

--- src/modules/recurrence/recurrence.repository.ts
+++ src/modules/recurrence/recurrence.repository.ts
@@ -1,11 +1,16 @@
 import {Inject, Injectable} from '@nestjs/common'
-import {Recurrence, RecurrenceModel} from './recurrence.model'
+import {
+  Recurrence,
+  RecurrenceModel,
+  RecurrenceWithTask
+} from './recurrence.model'
 import * as TE from 'fp-ts/lib/TaskEither'
 import {Transaction} from 'objection'
 import {TaskId} from '../tasks/task.model'
 import {InsertableRule} from './utils/map-to-insertable-rule'
 import {TasksRepo} from '../tasks/tasks.repository'
 import {UserError} from 'src/filters/user-error.exception.filter'
+import {transformRejectReason} from '../../shared/transform-reject-reason'
 
 @Injectable()
 export class RecurrenceRepo {
@@ -67,5 +72,18 @@ export class RecurrenceRepo {
       source_task_id: taskId,
       author_uid: uid
     })
+  }
+
+  findByNextDateWithGraphFetched(
+    date: Date
+  ): TE.TaskEither<UserError, RecurrenceWithTask[]> {
+    return TE.tryCatch(() => {
+      const query = this.recurrenceModel
+        .query()
+        .where({next_date: date.toISOString()})
+        .withGraphFetched('task')
+
+      return (query as unknown) as Promise<RecurrenceWithTask[]>
+    }, transformRejectReason)
   }
 }
