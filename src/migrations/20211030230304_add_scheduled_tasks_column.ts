f1d3f4198727bbc6cfc444e49d503974314b0bb0
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-30 23:10:54 +0300

    feat: add task date

---
+++ src/migrations/20211030230304_add_scheduled_tasks_column.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('tasks', t => {
+    t.date('date').defaultTo(null)
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('tasks', t => {
+    t.dropColumn('date')
+  })
+}
