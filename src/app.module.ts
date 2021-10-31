48b624b7f7bff05d75d3b2bb9f8d4507cd168622
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-10-31 17:43:11 +0300

    feat: async register objection module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import {Module} from '@nestjs/common'
 import {ScheduleModule} from '@nestjs/schedule'
-import {ConfigModule} from '@nestjs/config'
-import {
-  ObjectionModule,
-  ObjectionModuleOptions
-} from '@willsoto/nestjs-objection'
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {ObjectionModule} from '@willsoto/nestjs-objection'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 import {TasksModule} from './modules/tasks/tasks.module'
@@ -16,28 +13,34 @@ import {TaskSchedulerModule} from './modules/task-scheduler/task-scheduler.modul
 import {ReportsModule} from './modules/reports/reports.module'
 import {RecurrenceModule} from './modules/recurrence/recurrence.module'
 
-const objectionOptions: ObjectionModuleOptions = {
-  config: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: {
-      database: process.env.POSTGRES_DB,
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
-  }
-}
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validateEnv
     }),
-    ObjectionModule.register(objectionOptions),
+    ObjectionModule.registerAsync({
+      imports: [ConfigModule.forRoot({validate: validateEnv})],
+      inject: [ConfigService],
+      useFactory(config: AppConfigService) {
+        return {
+          config: {
+            client: 'pg',
+            useNullAsDefault: true,
+            connection: {
+              database: config.get('POSTGRES_DB'),
+              host: config.get('POSTGRES_HOST'),
+              user: config.get('POSTGRES_USER'),
+              password: config.get('POSTGRES_PASSWORD'),
+              port: config.get('POSTGRES_PORT'),
+              ssl: config.get('POSTGRES_SSL') as boolean
+            },
+            migrations: {
+              tableName: 'migrations'
+            }
+          }
+        }
+      }
+    }),
     ScheduleModule.forRoot(),
     TasksModule,
     ChecklistModule,
