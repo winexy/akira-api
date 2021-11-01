f12385893a7fc55da8c4fe830e0abbc9538029a2
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-11-01 21:18:17 +0300

    feat: add ping endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,9 @@
-import {Controller, Get, UseGuards} from '@nestjs/common'
-import {AppService} from './app.service'
-import {AuthGuard} from './auth.guard'
+import {Controller, Get} from '@nestjs/common'
 
 @Controller()
-@UseGuards(AuthGuard)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('ping')
+  ping(): string {
+    return 'pong'
   }
 }
