c8272055805de9443ef0637418edd1d2870dfa81
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-11-06 00:58:56 +0300

    feat: set cloned task date as today

--- src/modules/tasks/utils/get-clone-task-payload.ts
+++ src/modules/tasks/utils/get-clone-task-payload.ts
@@ -1,5 +1,6 @@
 import {pick, merge} from 'fp-ts-std/ReadonlyRecord'
 import {pipe} from 'fp-ts/lib/function'
+import {format} from 'date-fns'
 import {TaskT} from '../task.model'
 
 const CLONEABLE_PROPERTIES = [
@@ -8,11 +9,14 @@ const CLONEABLE_PROPERTIES = [
   'is_important',
   'list_id',
   'recurrence_id',
-  'title',
-  'date'
+  'title'
 ] as const
 
 const extractPoperties = pick<TaskT>()(CLONEABLE_PROPERTIES)
 
 export const getCloneTaskPayload = (task: TaskT) =>
-  pipe(task, extractPoperties, merge({source_task_id: task.id}))
+  pipe(
+    task,
+    extractPoperties,
+    merge({source_task_id: task.id, date: format(new Date(), 'yyyy-MM-dd')})
+  )
