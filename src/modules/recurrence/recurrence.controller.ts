36c4722426f79a261f3f2c15fca3207f4f2e29f4
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-11-05 14:58:26 +0300

    feat: add super user hook for recurrence worker

--- src/modules/recurrence/recurrence.controller.ts
+++ src/modules/recurrence/recurrence.controller.ts
@@ -6,11 +6,22 @@ import {ruleSchema, RuleSchema} from './recurrence.model'
 import {TaskId} from '../tasks/task.model'
 import {User} from 'src/decorators/user.decorator'
 import * as E from 'fp-ts/lib/Either'
+import {SuperUserGuard} from 'src/superuser.guard'
+import {RecurrenceWorker} from './recurrence.worker'
 
 @UseGuards(AuthGuard)
 @Controller('recurrence')
 export class RecurrenceController {
-  constructor(private readonly recurrenceService: RecurrenceService) {}
+  constructor(
+    private readonly recurrenceService: RecurrenceService,
+    private readonly recurrenceWorker: RecurrenceWorker
+  ) {}
+
+  @Post('hook')
+  @UseGuards(SuperUserGuard)
+  Hook() {
+    return this.recurrenceWorker.syncRecurrentTasks()
+  }
 
   @Post(':taskId')
   async create(
